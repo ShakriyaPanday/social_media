842e09e26b2e2f00ce6414095825617a7c059315
Shakriya Panday <fenom.shakriyapanday@gmail.com>
2022-12-01 12:07:05 +0545

    Login Screen added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:social_media_ui/screens/home_screen.dart';
+import 'package:social_media_ui/screens/login_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,11 +9,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Social Media',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: Colors.blue,
       ),
-      home: HomePage(),
+      home: const LoginPage(),
     );
   }
 }

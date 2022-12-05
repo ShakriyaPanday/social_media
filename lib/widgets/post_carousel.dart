d3e55dcee5b595c4152165a39bee6900479d082f
Shakriya Panday <fenom.shakriyapanday@gmail.com>
2022-12-05 21:28:16 +0545

    Pageview and image added

--- lib/widgets/post_carousel.dart
+++ lib/widgets/post_carousel.dart
@@ -12,8 +12,57 @@ class PageCarousel extends StatelessWidget {
       required this.title,
       required this.posts});
 
+  _buildPost(BuildContext context, int index) {
+    Post post = posts[index];
+    return Container(
+      margin: const EdgeInsets.all(10.0),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15.0),
+          boxShadow: const [
+            BoxShadow(
+              color: Colors.black26,
+              offset: Offset(0, 2),
+              blurRadius: 6.0,
+            )
+          ]),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(15.0),
+        child: Image(
+          height: 400.0,
+          width: 300.0,
+          image: AssetImage(post.imageUrl),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 10.0),
+          child: Text(
+            title,
+            style: const TextStyle(
+              fontSize: 24.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 400.0,
+          child: PageView.builder(
+              controller: pageController,
+              itemCount: posts.length,
+              itemBuilder: ((context, index) {
+                return _buildPost(context, index);
+              })),
+        )
+      ],
+    );
   }
 }
